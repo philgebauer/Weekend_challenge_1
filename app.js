3237c4fc26a9091988f964071ca9a5647f5482cc
Phil G <philg@Phils-MacBook-Pro.local>
2016-10-31 08:20:58 -0500

    changed styling

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 $(document).ready(function() {
     // console.log( "ready!" );
 
+
+    //init monthly info variables
     var monthlySalary = 0;
     var monthlyTotal = 0;
+
 
     $("#employeeinfo").on("submit", function(event) {
         event.preventDefault();
@@ -16,31 +19,34 @@ $(document).ready(function() {
           employee[element.name] = element.value;
         });
 
-        //console.log(employee);
+
 
         $('#employeeinfo').find('input[type=text]').val('');
 
         appendDom(employee);
 
           function appendDom(employee) {
-            $('#employeeTable').append('<tr class="person"></tr>');
+            $('#employeeTable').append('<tr class="person"></tr>');               // append to table
             var $el = $('#employeetable').children().last();
 
             $el.append(
                       '<tr class="listData"><td>' + employee.employeefirstname + ' ' +  employee.employeelastname +'</td><td>' + employee.employeeid + '</td><td>' + employee.employeejobtitle + '</td><td>'+ employee.employeesalary + '</td><td>' + '<button class="delete">Delete</button></td></tr>');
-
+                      //combine first and last names
+                      //added delete button at end
 
           employee.employeesalary = parseInt(employee.employeesalary);
 
           monthlySalary = parseInt(employee.employeesalary) / 12;
-          monthlyTotal = Math.round(monthlyTotal + monthlySalary);
+          monthlyTotal = Math.round(monthlyTotal + monthlySalary);        // calculate monthly salary from annual salary
 
+
+          //append monthly salary info to #money div
           $('.monthlyAmount').empty();
           $('#money').append('<p class ="monthlyAmount"> Monthly Salary Expenditures: ' + monthlyTotal + '$ </p>');
 
 
         }
-
+        //delete button
         $('.listData').click(function(){
           $(this).remove('.listData');
 
